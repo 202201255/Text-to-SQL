6c66cfbd84b14f7d635707b0856bdb099ef47cbd
Jay Rathod <202201255@daiict.ac.in>
2025-09-20 13:24:12 +0530

    Configure Gemini API and add response function

--- app.py
+++ app.py
@@ -1,4 +1,26 @@
+
+from dotenv import load_dotenv
+load_dotenv()
+
 import streamlit as st
+import os
+
+import google.generativeai as genai
+
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+
+# function to load google gemini model
+
+def get_gemini_response(question, prompt):
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    response = model.generate_content([prompt[0], question])
+    return response.text
+
+
+
+
+
 
 st.set_page_config(page_title="Text to SQL", page_icon="🔍")
 st.header("Gemini App to retrieve SQL data")
