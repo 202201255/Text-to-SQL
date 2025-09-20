76504b004eed04583aadf70e9e5653c857a803f9
Jay Rathod <202201255@daiict.ac.in>
2025-09-20 13:28:18 +0530

    Add SQLite query function and SQL prompt definition

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import streamlit as st
 import os
 
 import google.generativeai as genai
-
+import sqlite3
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
 
@@ -18,8 +18,31 @@ def get_gemini_response(question, prompt):
     return response.text
 
 
+# function to retrieve data from sqlite database
 
 
+def read_sql_query(sql, db):
+    connection = sqlite3.connect(db)
+    cur = connection.cursor()
+    cur.execute(sql)
+    rows = cur.fetchall()
+    connection.commit()
+    connection.close()
+    return rows
+
+
+prompt = [
+    """
+    You are an expert in converting English questions to SQL code!
+    The SQL database has the name STUDENT and has the following columns - NAME, PROGRAM, SECTION. \n\n
+    For example, 
+    \n example 1 - How many entries of records are present? ,
+    the SQL command will be something like this SELECT COUNT(*) FROM STUDENT
+    \n example 2 - give me all students studying in Data Science class? ,
+    the SQL command will be something like this SELECT * FROM STUDENT where PROGRAM='Data science'
+    Also the SQL code should not have ``` in beginning or end and sql word in output.
+    """
+]
 
 
 st.set_page_config(page_title="Text to SQL", page_icon="🔍")
